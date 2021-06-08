91c7c64a39245472e8fd6d36f5e4decb369686c8
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-08 20:54:16 +0700

    xu ly dong bo luong

--- src/main.java
+++ src/main.java
@@ -3,27 +3,38 @@ import java.util.TimerTask;
 import java.util.logging.Handler;
 
 public class main {
+    static int a = 0 ,b = 0 , c = 0;
     public static void main (String[] args) throws Exception{
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                handleLogic("A");
+                for (int i = 1; i <= 50 ; i++) {
+                    a = i;
+                    System.out.println("A = " + a);
+                }
             }
         });
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                handleLogic("B");
+                for (int i = 1; i <= 50 ; i++) {
+                    b = i;
+                    System.out.println("B = " + b);
+                }
             }
         });
+        Thread threadC = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 50 ; i++) {
+                    c = a + b;
+                    System.out.println("C = " + c);
+                }
+            }
+        });
+
         threadA.start();
         threadB.start();
-
-        // xử đồng bộ mỗi lần chạy chỉ  1 thread chạy
-    }
-    private synchronized static void handleLogic(String name){
-        for (int i = 0; i < 200 ; i++) {
-            System.out.println(name + " " + i);
-        }
+        threadC.start();
     }
 }
