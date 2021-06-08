68e2b0e89e82ebf52bce00936b8a3ba3a2b741a5
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-08 21:27:34 +0700

    finish

---
+++ src/MyFlag.java
@@ -0,0 +1,7 @@
+public class MyFlag {
+    int count;
+
+    public MyFlag(int count) {
+        this.count = count;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,34 +1,78 @@
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.logging.Handler;
+
 
 public class main {
-    static int a = 0 ,b = 0 , c = 0;
-    public static void main (String[] args) throws Exception{
+    static int a = 0, b = 0, c = 0;
+
+    public static void main(String[] args) throws Exception {
+        MyFlag myFlag = new MyFlag(0);
+
+//        0 => A chạy;
+//        1 => B chạy;
+//        2 => C chạy;
+
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 1; i <= 50 ; i++) {
-                    a = i;
-                    System.out.println("A = " + a);
+                synchronized (myFlag) {
+                    for (int i = 1; i <= 50;) {
+                        if (myFlag.count == 0) {
+                            a = i;
+                            System.out.println("A = " + a);
+                            myFlag.count = 1;
+                            myFlag.notifyAll();
+                            i++;
+                        } else {
+                            try {
+                                myFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
                 }
             }
         });
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 1; i <= 50 ; i++) {
-                    b = i;
-                    System.out.println("B = " + b);
+                synchronized (myFlag) {
+                    for (int i = 1; i <= 50;) {
+                        if (myFlag.count == 1) {
+                            b = i;
+                            System.out.println("B = " + b);
+                            myFlag.count = 2;
+                            myFlag.notifyAll();
+                            i++;
+                        } else {
+                            try {
+                                myFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
                 }
             }
         });
         Thread threadC = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 1; i <= 50 ; i++) {
-                    c = a + b;
-                    System.out.println("C = " + c);
+                synchronized (myFlag) {
+                    for (int i = 1; i <= 50;) {
+                        if (myFlag.count == 2) {
+                            c = a + b;
+                            System.out.println("C = " + c);
+                            myFlag.count = 0;
+                            myFlag.notifyAll();
+                            i++;
+                        } else {
+                            try {
+                                myFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
                 }
             }
         });
